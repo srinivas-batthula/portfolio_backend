affdbd117979c33b45f36af62f928a25f62052c2
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2024-11-10 10:59:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const email = require('./services/email');
+const cors = require('cors');
 
 const app =express();
+
+app.use(cors());
 
 app.use(express.json());
 
